fd009091b11c5a675ffd276cd6139e8b7340cadd
joshuajpiluden@gmail.com <joshuajpiluden@Gmail.com>
2024-02-11 11:26:41 +0800

    Added icons

--- src/views/AboutMeView.vue
+++ src/views/AboutMeView.vue
@@ -5,10 +5,12 @@
       <p>Just a regular .NET developer who loves exploring new stuff.</p>
     </div>
     <div class="flex  ml-20">
-      <img class="object-fill h-10 w-10 mr-5 transition duration-300 hover:translate-x-2" src="/src/assets/c-sharp.png"
+      <img class="object-fill h-10 w-10 mr-5 transition duration-300 hover:translate-x-2" src="/src/assets/c-sharp.svg"
         title="C#" />
-      <img class="object-fill h-10 w-10 mr-5 transition duration-300 hover:translate-x-2" src="/src/assets/azure.png"
+      <img class="object-fill h-10 w-10 mr-5 transition duration-300 hover:translate-x-2" src="/src/assets/azure.svg"
         title="Azure" />
+      <img class="object-fill h-10 w-10 mr-5 transition duration-300 hover:translate-x-2" src="/src/assets/dotnet.svg"
+        title=".NET" />
       <img class="object-cover h-10 w-10 mr-5 transition duration-300 hover:translate-x-2"
         src="/src/assets/PowerApps_scalable.svg" title="Power Apps" />
       <img class="object-cover h-10 w-10 mr-5 transition duration-300 hover:translate-x-2"
