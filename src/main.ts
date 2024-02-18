8497fbee022a080d30d575ae52a57ace5d461410
joshuajpiluden@gmail.com <joshuajpiluden@Gmail.com>
2024-02-18 18:41:04 +0800

    Added google analytics

--- src/main.ts
+++ src/main.ts
@@ -3,15 +3,25 @@ import { createPinia } from 'pinia';
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { fab } from '@fortawesome/free-brands-svg-icons';
-
-
-library.add(fab);
 import App from './App.vue';
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
 import './index.css';
-
-
 import router from './router';
 
+library.add(fab);
+
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_MEASUREMENT_ID
+};
+const firebaseApp = initializeApp(firebaseConfig);
+getAnalytics(firebaseApp);
 const app = createApp(App);
 app.component('font-awesome-icon', FontAwesomeIcon);
 app.use(createPinia());
